3b87cb08a92fa81ca6bea5b0f5eb09f976e4de71
mikaelkiefer <mkdcatral@gmail.com>
2022-02-07 23:22:10 +0400

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,28 @@
+ec = ec || {};
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
+
+ec.order.extraFields.tips = {
+    'title': ‘COD Fee,
+    'type': 'toggleButtonGroup',
+    'options': [
+    {
+      'title': ‘Cash’ on Delivery Fee,
+      'subtitle': ‘Pay online and save COD Cost,
+      'surcharge': 15
+    }
+  ],
+  'surchargeType': ‘A’BSOLUTE,
+  'surchargeShortName': {
+    'name': ‘COD Fee’,
+    'showSurchargePercentValue': false,
+    'nameTranslated': {
+      'en': 'COD Fee',
+    }
+  },
+  'showZeroSurchargeInTotal' : false,
+  'required': true,
+  'checkoutDisplaySection': 'payment_details'
+};
+
+Ecwid.refreshConfig && Ecwid.refreshConfig()
