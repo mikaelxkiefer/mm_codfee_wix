a97bb38a836e7fde9cd554b59cf4667e73386e41
mikaelkiefer <mkdcatral@gmail.com>
2022-02-14 17:56:06 +0400

    myscript_v3.1.js

--- myscript.js
+++ myscript.js
@@ -1,28 +1,57 @@
-ec = ec || {};
-ec.order = ec.order || {};
-ec.order.extraFields = ec.order.extraFields || {};
+Ecwid.OnPageLoaded.add(function (page) {
+    if (page.type == "CHECKOUT_PAYMENT_DETAILS") {
+        //initialize extra fields
+        ec = ec || {};
+        ec.order = ec.order || {};
+        ec.order.extraFields = ec.order.extraFields || {};
 
-ec.order.extraFields.tips = {
- 'title': 'COD Fee',
- 'type': 'toggleButtonGroup',
- 'options': [
-    {
- 'title': 'Cash on Delivery Fee',
- 'subtitle': 'Pay online and save COD Cost',
- 'surcharge': 15
-    }
-  ],
- 'surchargeType': 'ABSOLUTE',
- 'surchargeShortName': {
- 'name': 'COD Fee',
- 'showSurchargePercentValue': false,
- 'nameTranslated': {
- 'en': 'COD Fee',
-    }
-  },
- 'showZeroSurchargeInTotal' : false,
- 'required': true,
- 'checkoutDisplaySection': 'payment_details'
-};
+        //wait for changes in the DOM
+        const targetNode = document.querySelector('.ec-cart-step--payment .ec-radiogroup__item');
+        const observerOptions = {
+            childList: true,
+            subtree: true
+        };
 
-Ecwid.refreshConfig && Ecwid.refreshConfig()
+        let observer = new MutationObserver(callback);
+        observer.observe(targetNode, observerOptions);
+
+        //function to apply the fee
+        function callback() {
+            let payment = document.querySelector('.ec-radiogroup__item--checked .ec-radiogroup__title');
+            if (payment.innerHTML === 'Cash on Delivery') {
+                ec.order.extraFields.surcharge = {
+                    'value': 'COD Fee',
+                    "options": [
+                        {
+                            "title": "COD Fee",
+                            "surcharge": 15
+                        },
+                    ],
+                    "surchargeShortName": {
+                        "name": "COD Fee",
+                        "showSurchargePercentValue": false
+                    },
+                    'surchargeType': 'ABSOLUTE',
+                }
+                Ecwid.refreshConfig();
+            } else {
+                ec.order.extraFields.surcharge = {
+                    'value': 'Fee',
+                    "options": [
+                        {
+                            "title": "Fee",
+                            "surcharge": 0
+                        },
+                    ],
+                    "surchargeShortName": {
+                        "name": "Fee",
+                        "showSurchargePercentValue": false
+                    },
+                    'surchargeType': 'PERCENT',
+                    'showZeroSurchargeInTotal': false
+                }
+                Ecwid.refreshConfig();
+            }
+        }
+    }
+});
